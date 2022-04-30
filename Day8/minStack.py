9ca0235ef82bd2f6f9730486879f777644fcd821
Shulav Karki <40908371+shulavkarki@users.noreply.github.com>
2022-04-30 23:20:34 +0545

    minStack: stack problem

---
+++ Day8/minStack.py
@@ -0,0 +1,53 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.minimum = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        if not self.minimum:
+            self.minimum.append(val)
+        else:
+            temp = self.minimum[len(self.minimum)-1]
+            if temp>=val:
+                self.minimum.append(val)
+            else:
+                self.minimum.append(temp)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack and self.minimum:
+            self.stack.pop()
+            self.minimum.pop()
+            
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        length = len(self.stack)
+        return self.stack[length - 1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minimum[len(self.minimum)-1]
+        
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
