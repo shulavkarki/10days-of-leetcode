cb48b2d09e50e18e478a5db1d79ddbf68ef4a3d8
Shulav Karki <40908371+shulavkarki@users.noreply.github.com>
2022-05-03 22:01:56 +0545

    may leetcode challenge Day1

---
+++ Day8/generateValidParanthesis.py
@@ -0,0 +1,25 @@
+def generateParanthesis(num_pair):
+    openN = num_pair
+    closeN = num_pair
+    tempStr = []
+    res = []
+
+    def backtrack(openN, closeN, tempStr, res):
+        if openN==closeN==0:
+            res.append("".join(tempStr))
+            return 
+        if openN!=0:
+            tempStr.append("(")
+            # openN-=1
+            backtrack(openN-1, closeN, tempStr, res)
+            tempStr.pop()
+        if closeN>openN:
+            tempStr.append(")")
+            # openN-=1
+
+            backtrack(openN, closeN-1,  tempStr, res)
+            tempStr.pop()
+
+    backtrack(openN, closeN, tempStr, res)
+    return res
+print(generateParanthesis(3))
